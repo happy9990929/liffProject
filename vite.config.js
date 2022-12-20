7cb191488fb2f271b6f46d6a8227fbc1abdce907
Lala <lala9990929@gmail.com>
2022-12-20 20:19:33 +0800

    base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: import.meta.env.VITE_LIFF_ID,
+  base: './liffProject',
   plugins: [vue()],
 })
