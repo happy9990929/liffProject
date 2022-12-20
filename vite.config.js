74ecaeae94e90a23b552183337ef41a6742e94de
Lala <lala9990929@gmail.com>
2022-12-20 20:35:44 +0800

    bose env

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/liffProject/',
+  base: import.meta.env.VITE_BASE_URL,
   plugins: [vue()],
 })
